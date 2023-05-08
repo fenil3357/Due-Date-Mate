c2b57d2cdbd4447af71896f0f15882b461c54658
Fenil <fenil3357@gmail.com>
2023-05-08 11:44:06 +0530

    Token Validation middleware added

--- api/src/middleware/token_validation.js
+++ api/src/middleware/token_validation.js
@@ -1,27 +1,32 @@
 const jwt = require("jsonwebtoken");
 
-
 module.exports = {
   checkToken: (req, res, next) => {
+    if(req.url.includes('/auth/')) {
+      next();
+      return;
+    }
     let token = req.get("authorization");
     if (token) {
       // Remove Bearer from string
       token = token.slice(7);
-      jwt.verify(token,"this is my secret key", (err, decoded) => {
+      jwt.verify(token,process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-          return res.json({
-            success: 0,
-            message: "Invalid Token..."
+          return res.status(403).json({
+            Error : "Access Denied",
+            ERR_CODE: "JWT_ERROR",
+            status: false
           });
         } else {
-          req.decoded = decoded;
+          req.data = decoded;
           next();
         }
       });
     } else {
-      return res.json({
-        success: 0,
-        message: "Access Denied! Unauthorized User"
+      return res.status(403).json({
+        Error : "Access Denied",
+        ERR_CODE: "JWT_ERROR",
+        status: false
       });
     }
   }
